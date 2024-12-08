f194963dfa5f5e23f543ecb53e03415bb67c9b21
Damiano Nicolussi <68129014+damnicolussi@users.noreply.github.com>
2024-12-08 18:47:33 +0100

    fixed warning messages

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,14 +1,15 @@
-use wg_2024::tests::{generic_fragment_forward, generic_fragment_drop};
-use drone_BetterCallDrone::drone::BetterCallDrone;
+#[warn(non_snake_case)]
+use wg_2024::tests::{generic_fragment_forward, generic_fragment_drop, generic_chain_fragment_drop};
+use drone_bettercalldrone::drone::BetterCallDrone;
 
 #[test]
-fn test_forwarding() {
+fn test_generic_fragment_forward() {
     println!("testing 1");
     generic_fragment_forward::<BetterCallDrone>();
 }
 
 #[test]
-fn test_packet_drop() {
+fn test_generic_fragment_drop() {
     println!("testing 2");
     generic_fragment_drop::<BetterCallDrone>();
 }
